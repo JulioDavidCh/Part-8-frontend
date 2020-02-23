3bfbf586c6470bbd606205f286f9403289eeeec5
Julio <juliodavidchopite@gmail.com>
2020-02-23 03:28:58 -0300

    added a logout button and made our login, logout and addbook navigation buttons only be shown when theres a token saved in localstorage

--- src/App.js
+++ src/App.js
@@ -2,20 +2,42 @@ import React, { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import Login from './components/Login'
 
 const App = () => {
   const [page, setPage] = useState('authors')
+  const [token, setToken] = useState(localStorage.getItem('loggedUser'))
 
   return (
     <div>
       <div>
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
-        <button onClick={() => setPage('add')}>add book</button>
+        {
+          token
+          ? <button onClick={() => setPage('add')}>add book</button>
+          : null
+        }
+        {
+          token
+          ? null
+          : <button onClick={() => setPage('login')}>login</button>
+        }
+        {
+          token
+          ? <button onClick={() =>{
+              localStorage.clear()
+              setToken(null)
+            }}>
+              logout
+            </button>
+          : null
+        }
       </div>
 
       <Authors
         show={page === 'authors'}
+        token={token}
       />
 
       <Books
@@ -25,7 +47,12 @@ const App = () => {
       <NewBook
         show={page === 'add'}
       />
-
+      
+      <Login
+        show={page === 'login'}
+        setOurPage={setPage}
+        setOurToken={setToken}
+      />
     </div>
   )
 }
